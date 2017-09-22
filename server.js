be26f9da907f6988a3ea99402d0a61c7f1caa63b
浪浪浪 <675483520@qq.com>
2017-09-22 12:42:56 +0800

    update port

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(function (err, req, res, next) {
 
 
 
-var port = normalizePort(process.env.PORT || '3000');
+var port = normalizePort(process.env.PORT || '8000');
 app.set('port', port);
 
 var server = http.createServer(app);
